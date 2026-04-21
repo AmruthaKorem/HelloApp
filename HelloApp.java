78b8cd77b9a7e5f5544f151eeb0bbd5938ae5a4c
Amrutha Korem <korem.amrutha@gmail.com>
2026-04-21 15:48:27 +0530

    Add files via upload

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,22 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+
+            System.out.print("Hello, ");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+
+            System.out.println("!");
+        } 
+        else {
+            System.out.println("Hello, World!");
+        }
     }
 }
